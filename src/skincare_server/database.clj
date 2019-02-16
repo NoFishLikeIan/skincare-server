4a5d44ccff05432fedad9684ae045a7e00f11080
Andrea Titton <andrea.titton.ext@accurat.it>
2019-02-17 00:52:00 +0100

    :card_file_box: connected mongodb

---
+++ src/skincare_server/database.clj
@@ -0,0 +1,22 @@
+(ns skincare_server.database
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [dotenv :refer [env]])
+  (:import [org.bson.types ObjectId]))
+
+;; Connects to default port
+(defn mongo-pusher-factory
+  "A factory that returns a function that pushes onto mongo"
+  [& [dbname collection-name]]
+  (let [dbname (or dbname (env "DB_NAME"))
+        collection-name (or collection-name (env "COLLECTION_NAME"))
+        conn (mg/connect)
+        db (mg/get-db conn dbname)]
+    (fn [post-data]
+      (let [post (assoc post-data :_id (ObjectId.))
+            document (mc/insert-and-return db collection-name post)]
+        (println document)
+        document))))
+
+
+(def default-push (mongo-pusher-factory))
